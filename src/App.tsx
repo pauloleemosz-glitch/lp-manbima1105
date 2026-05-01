a866fac9de0161459f3c86f83dc5a2f7d82589e3
Paulo Lemos <pauloleemosz@gmail.com>
2026-05-01 07:36:02 -0300

    add /manbima-convite route alias for captacao page

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<CaptacaoPage />} />
+        <Route path="/manbima-convite" element={<CaptacaoPage />} />
         <Route path="/parabens-manbima" element={<ObrigadoPage />} />
       </Routes>
     </BrowserRouter>
